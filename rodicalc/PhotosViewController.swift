62563447ea5799a6efffced94d2966c6d7afd3e0
deck <deck@imac-pimenov-2.local>
2016-03-30 18:02:05 +0300

    add photo from gallery add photo from camera

--- rodicalc/PhotosViewController.swift
+++ rodicalc/PhotosViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class PhotosViewController: UICollectionViewController {
-
+class PhotosViewController: UICollectionViewController, UIImagePickerControllerDelegate,UIPopoverControllerDelegate,UINavigationControllerDelegate {
+    var choosedSegment = true // true: photo, false: uzi
+    var picker:UIImagePickerController?=UIImagePickerController()
+    var photos = [UIImage]()
+    var uzis = [UIImage]()
+    @IBOutlet var PhotoCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        picker?.delegate=self
         let a = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: #selector(PhotosViewController.openCamera))
         let b = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(PhotosViewController.addPhoto))
         a.tintColor = UIColor.whiteColor()
@@ -21,12 +25,65 @@ class PhotosViewController: UICollectionViewController {
     }
 
     func openCamera(){
-
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            picker!.allowsEditing = false
+            picker!.sourceType = UIImagePickerControllerSourceType.Camera
+            picker!.cameraCaptureMode = .Photo
+            presentViewController(picker!, animated: true, completion: nil)
+        }else{
+            let alert = UIAlertController(title: "Camera Not Found", message: "This device has no Camera", preferredStyle: .Alert)
+            let ok = UIAlertAction(title: "OK", style:.Default, handler: nil)
+            alert.addAction(ok)
+            presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
     func addPhoto(){
-        
+        picker!.allowsEditing = false
+        picker!.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(picker!, animated: true, completion: nil)
     }
+    
+    @IBAction func BtnSelect(sender: AnyObject) {
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photos.append(chosenImage)
+        dismissViewControllerAnimated(true, completion: nil)
+        PhotoCollectionView.reloadData()
+    }
+    
+    @IBAction func SegmentChanger(sender: AnyObject) {
+        self.reloadTable(sender.selectedSegmentIndex == 1 ? false : true)
+    }
+    private func reloadTable(index: Bool) {
+        choosedSegment = index
+        PhotoCollectionView.reloadData()
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return  choosedSegment ? photos.count : uzis.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let PhotoCell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) as! VideoCollectionViewCell
+        //PhotoCell.photo.image = choosedSegment ? photos[indexPath.row] : uzis[indexPath.row]
+        return PhotoCell
+    }
+    
+    
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
