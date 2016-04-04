6dcc98aeef455e5882af15b432bee581b6e9152e
deck <deck@imac-pimenov-2.local>
2016-04-04 15:26:41 +0300

    add json with zodiac sign add read zodiac sign json file

--- rodicalc/CalcTableViewController.swift
+++ rodicalc/CalcTableViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
+class Zodiac: NSObject {
+    var name: String
+    var element: String
+    var about: String
+    init(name: String, element: String, about: String) {
+        self.name = name
+        self.element = element
+        self.about = about
+        super.init()
+    }
+}
+
 class CalcViewController: UIViewController {
 
+    var zodiacs = [Zodiac]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        WorkWithJSON()
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
@@ -25,14 +39,26 @@ class CalcViewController: UIViewController {
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func WorkWithJSON(){
+        if let path = NSBundle.mainBundle().pathForResource("zodiacs", ofType: "json") {
+            do {
+                let jsonData = try NSData(contentsOfFile: path, options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                do {
+                    let jsonResult: NSDictionary = try NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                    if let zodiac : [NSDictionary] = jsonResult["Знаки"] as? [NSDictionary] {
+                        for Zodiacs: NSDictionary in zodiac {
+                            var name = Zodiacs.valueForKey("Знак")
+                            name!.dataUsingEncoding(NSUTF8StringEncoding)
+                            if let d = name {
+                                //points.append(Points(city: "\(Point.valueForKey("city"))",address: d as! String,trade_point: "\(Point.valueForKey("trade_point")!)",phone: "\(Point.valueForKey("phone")!)",longitude: (Point.valueForKey("coord_last_latitude") as? Double)! ,latitude:(Point.valueForKey("coord_first_longtitude") as? Double)!))
+                                zodiacs.append(Zodiac(name: d as! String, element: "\(Zodiacs.valueForKey("Стихия"))", about: "\(Zodiacs.valueForKey("Описание"))"))
+                            }
+                        }
+                    }
+                } catch {}
+            } catch {}
+        }
     }
-    */
+
 
 }
